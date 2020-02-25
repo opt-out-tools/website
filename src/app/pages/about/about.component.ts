2edf7ce62897e1a9756d6c2e72cad4eca6c72245
Nikola Ribic <nikola.ribic@softerrific.com>
2020-02-25 16:04:46 +0100

    - enabled scroll on home, and updated global variables
    - added global variables for side margin, header and footer height
    - setup scroll for articles on home page
    - updated css for home page for scrolling

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-about',
