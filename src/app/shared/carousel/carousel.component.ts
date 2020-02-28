f6a0775276ed1aad180d6384706cd442747f3457
Nikola Ribic <nikola.ribic@softerrific.com>
2020-02-28 17:56:57 +0100

    - added additional quotes

--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -40,6 +40,45 @@ export class CarouselComponent implements AfterViewInit {
       title: 'Data & Society',
       text: '47% of internet users (aged 15 and older) have experienced online harassment or abuse, and 72% have witnessed it.'
     },
+    {
+      title: 'Amnesty International',
+      text: 'Online abuse has a silencing or censoring effect on women with more than 3/4 (76%) of women across the eight countries who ' +
+        'had experienced abuse or harassment on social media making some changes to the way they use social media platforms as a result.',
+    },
+    {
+      title: 'Amnesty International',
+      text: 'Of the women polled who have experienced online abuse or harassment: one-third (36%) felt their physical safety had been ' +
+        'threatened; \tmore than half (55%) suffered stress, anxiety or panic attacks; three out of five (61%) had trouble sleeping ' +
+        'well; two-thirds (67%) felt apprehensive when thinking about using social media; and one in five (20%) felt the online abuse ' +
+        'threatened their job prospects.'
+    },
+    {
+      title: 'PEW Research Center',
+      text: 'More than a quarter of Americans have chosen to not post something online after seeing harassment of others'
+    },
+    {
+      title: 'Jessica Valenti',
+      text: 'I think Twitter is the worst of the social media platforms, just because of the quickened and masked flow [of abuse] that ' +
+        'happens. The content feels pretty similar across the platforms but the sheer volume of it on Twitter is what’s different.'
+    },
+    {
+      title: 'Amnesty International',
+      text: 'The violence and abuse many women experience on Twitter has a detrimental effect on their right to express themselves ' +
+        'equally, freely and without fear. Instead of strengthening women’s voices, the violence and abuse many women experience on the ' +
+        'platform leads women to self-censor what they post, limit their interactions, and even drives women off Twitter completely.'
+    },
+    {
+      title: 'Data & Society',
+      text: 'Young people, women, and lesbian, gay, and bisexual (LGB) Americans are more likely to experience online harassment and ' +
+        'more likely to self-censor online postings over concerns about attracting harassment.'
+    },
+    {
+      title: 'UN Special Rapporteur on VAW',
+      text: 'It’s “important to acknowledge that the Internet is being used in a broader environment of widespread and systemic ' +
+        'structural discrimination and gender-based violence against women and girls, which frame their access to and use of the ' +
+        'Internet and other ICT.'
+    }
+
   ];
 
   onPreviousClick() {
